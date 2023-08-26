7746e28dc91ec9cce5b78796fec65cd0116b07d2
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-26 08:59:12 +0000

    adds a functioning user registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,8 @@ class UsersController < ApplicationController
     end
   end
 
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
 end
