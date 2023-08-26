739191622280d27ae96dc835fb83ffd6441471f2
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-26 08:16:14 +0000

    adds create method for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { message: 'User created successfully' }, status: :created
+    else 
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  
 end
