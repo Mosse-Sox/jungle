f144b78cc440cfcd8dd2dcce0b12d1822159185a
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-26 11:44:05 +0000

    adds login functionality, adds users firstname and a logout button to nav when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path
+    else 
+      flash.now[:alert] = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
 end
