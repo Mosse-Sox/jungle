94b475774bb3e3aaacdfd053093dc8acb3f30d75
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-29 02:37:26 +0000

    adds tests for products model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,5 +9,32 @@ RSpec.describe Product, type: :model do
       
       expect{ product.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
     end
+
+    it 'should not save if the price field is nil' do
+      category = Category.create(name: 'Sample Category')
+      product = Product.new(name: 'Bean', price: nil, quantity: 50, category: category)
+      
+      expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Price can't be blank")
+    end
+
+    it 'should not save if the category field is nil' do
+      product = Product.new(name: 'Bean', price: 500, quantity: 50, category: nil)
+      
+      expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Category must exist, Category can't be blank")
+    end
+
+    it 'should not save if the quantity field is nil' do
+      category = Category.create(name: 'Sample Category')
+      product = Product.new(name: 'Bean', price: 500, quantity: nil, category: category)
+      
+      expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Quantity can't be blank")
+    end
+
+    it 'should save if all fields are correctly filled' do
+      category = Category.create(name: 'Sample Category')
+      product = Product.new(name: 'Bean', price: 500, quantity: 50, category: category)
+      
+      expect { product.save! }.not_to raise_error
+    end
   end
 end
