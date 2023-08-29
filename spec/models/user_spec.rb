d2377985c18c1c0585290b6ff65659e4034fc4a9
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-29 03:10:25 +0000

    adds tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe User, type: :model do
 
       expect { user.save! }.not_to raise_error
     end
+
+    it 'should not save if the first_name field is nil' do
+      user = User.create(first_name: nil, last_name: 'Beans', email: 'roguy99@gmail.ca', password: 'BeefBurrito')
+      
+      expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: First name can't be blank")
+    end
+
+    it 'should not save if the last_name field is nil' do
+      user = User.create(first_name: 'Rowan', last_name: nil, email: 'roguy99@gmail.ca', password: 'BeefBurrito')
+      
+      expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Last name can't be blank")
+    end
   end
 end
