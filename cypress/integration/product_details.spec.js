46ed132b8efd29fe1db3cd4ab83344c618bc25b8
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-30 00:43:11 +0000

    adds tests for product-details page to ensure its loading and working correctly

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,23 @@
+describe("Product Details Page", () => {
+  it("should visit the home page", () => {
+    cy.visit("/");
+  });
+
+  it("A user should be able to click a product and navigate to the product details page", () => {
+    cy.get(".products article").first().click();
+
+    cy.url().should("include", "/products/2");
+    cy.get("h1").contains("Scented Blade");
+  });
+
+  it("A user should be able to add the product to their carts from the product details page", () => {
+    cy.get(".btn").contains(" Add ").should("exist").click();
+    cy.wait(5000);
+    cy.get(".nav-link").contains("My Cart (1)");
+  });
+
+  it("A user should be able to navigate back to the home page from the product details page", () => {
+    cy.get(".navbar-brand").contains("Jungle").click();
+    cy.location("pathname").should("eq", "/");
+  });
+});
